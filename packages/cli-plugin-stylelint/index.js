2668e87639f371079f5d1c78bbea8af61f7b78bc
nolonger21 <nolonger@qq.com>
2021-02-01 00:55:49 +0800

    chore(pkg): add cli-plugin-stylelint pkg

---
+++ packages/cli-plugin-stylelint/index.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = (api, options, pluginConfig) => {
+  const { error, info, warn, resolveModule, loadModule, tryRequire } = require('@etherfe/cli-utils')
+  const cwd = api.getCwd()
+  const eslintPkg = loadModule('stylelint/package.json', cwd, true)
+
+  if (!eslintPkg) {
+    error( `The project seems to require 'stylelint' but it's not installed.`)
+    process.exit(1)
+  }
+
+  api.chainWebpack(chainWebpack => {
+    chainWebpack
+      .plugin('stylelint')
+        .use(require.resolve('stylelint-webpack-plugin'), [{
+          context: api.resolve('src'),
+          files: ['**/*.{html,vue,css,sass,scss,less,styl}'],
+          cache: true,
+          cacheLocation: api.resolve('node_modules/.cache/.stylelintcache/'),
+          ...pluginConfig.stylelintOptions
+        }])
+  })
+
+}
+
+module.exports.defaultConfig = {
+  stylelintOptions: {
+    cache: true,
+    fix: false,
+    emitErrors: true,
+    failOnError: false,
+    lintDirtyModulesOnly: false
+  }
+}
