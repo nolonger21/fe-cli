bf5597232bb05fd4fc4a4d6051f8465d3cec46ca
nolonger21 <nolonger@qq.com>
2021-02-01 01:00:23 +0800

    fix: add rule and note

--- packages/eslint-plugin/configs/prettier.js
+++ packages/eslint-plugin/configs/prettier.js
@@ -4,7 +4,15 @@ module.exports = {
     'prettier'
   ],
   rules: {
-    'prettier/prettier': [1, {}, {
+    'prettier/prettier': [1, {
+      // printWidth: 120,
+      // tabWidth: 2,
+      // useTabs: false,
+      // singleQuote: true, // -> project root .prettierrc.js
+      // semi: false,
+      // trailingComma: 'es5',
+      // bracketSpacing: true
+    }, {
       usePrettierrc: true
     }]
   }
--- packages/eslint-plugin/configs/vue.js
+++ packages/eslint-plugin/configs/vue.js
@@ -1,23 +1,32 @@
 
 
 module.exports = {
-    parser: require.resolve('vue-eslint-parser'),
-    parserOptions: {
-      parser: require.resolve('babel-eslint')
-    },
-    plugins: ['vue'],
-    extends: [
-      require.resolve('./recommended'),
-      'plugin:vue/recommended'
-    ],
-    rules: {
-      'vue/max-attributes-per-line': [2, {
-        "singleline": 8,
-        "multiline": {
-          "max": 1,
-          "allowFirstLine": false
-        }
-      }]
-    }
-  };
-  
+  parser: require.resolve('vue-eslint-parser'),
+  parserOptions: {
+    parser: require.resolve('babel-eslint')
+  },
+  plugins: ['vue'],
+  extends: [
+    require.resolve('./recommended'),
+    'plugin:vue/recommended'
+  ],
+  rules: {
+    'vue/max-attributes-per-line': [2, {
+      "singleline": 8,
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": false
+      }
+    }],
+    'vue/singleline-html-element-content-newline': 0,
+    "vue/html-self-closing": [1,{
+      "html": {
+        "void": "always",
+        "normal": "always",
+        "component": "always"
+      },
+      "svg": "always",
+      "math": "always"
+    }]
+  }
+};
