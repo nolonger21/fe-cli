5c548c119beb70541d5f93370e8574c0fa133fcf
nolonger21 <nolonger@qq.com>
2021-03-16 00:42:19 +0800

    fix: vue jsx babel plugin

--- packages/cli-plugin-vue3/index.js
+++ packages/cli-plugin-vue3/index.js
@@ -33,12 +33,12 @@ module.exports = (api, options, pluginConfig) => {
       .plugin('vue-loader')
       .use(require('vue-loader').VueLoaderPlugin)
 
-    if (api.hasPlugin('babel') && api.hasDepend('element-ui')) {
+    if (api.hasPlugin('babel')) {
       chainWebpack.module
         .rule('js')
         .use('babel-loader')
         .tap(options => {
-          options.plugins.push('@vue/babel-plugin-jsx')
+          options.plugins.push(require.resolve('@vue/babel-plugin-jsx'))
           options.plugins.push(
             [
               require.resolve('babel-plugin-component'),
