835c57e445b768d01607fc6fda8f87e7195de7bb
nolonger21 <nolonger@qq.com>
2021-03-18 00:58:08 +0800

    feat: add svg mini config

--- packages/cli-service/lib/config/assets.js
+++ packages/cli-service/lib/config/assets.js
@@ -6,16 +6,21 @@ module.exports = (api, options, pluginConfig) => {
     const outputAssetName = `${pluginConfig.filenameHashing ? '.[hash:8]' : ''}.[ext]`
 
     const genUrlLoaderOptions = dir => {
-      return {
+      const urlLoaderOptions = {
         limit: inlineLimit,
         fallback: {
           loader: require.resolve('file-loader'),
           options: {
             name: getAssetPath(pluginConfig.assetsDir, `${dir}/[name]${outputAssetName}`),
             esModule: false
-          }
+          },
         }
       }
+      if (pluginConfig.miniSvg) {
+        const svgToMiniDataURI = require('mini-svg-data-uri')
+        urlLoaderOptions.generator =  (content) => svgToMiniDataURI(content.toString())
+      }
+      return urlLoaderOptions
     }
 
     chainWebpack.module
@@ -43,5 +48,6 @@ module.exports = (api, options, pluginConfig) => {
 }
 
 module.exports.defaultConfig = {
-  inlineLimit: 4096
+  inlineLimit: 4096,
+  miniSvg: false
 }
