922f0bdec044dc5d11dda8946e1679399adbf6de
nolonger21 <nolonger@qq.com>
2021-03-22 01:41:12 +0800

    feat: upgrade eslint config

--- packages/eslint-plugin/configs/vue3-typescript.js
+++ packages/eslint-plugin/configs/vue3-typescript.js
@@ -1,40 +1,9 @@
 
-const vueConfig = require('./vue3')
-const typescriptConfig = require('./typescript');
-
-const typescriptOverrides = typescriptConfig.overrides[0]
+const vueTsConfig = require('./vue-typescript')
 
 module.exports = {
   extends: [
     require.resolve('./vue3')
   ],
-  overrides: [
-    {
-      files: typescriptOverrides.files.concat(['*.vue']),
-      parser: vueConfig.parser,
-      parserOptions: {
-        ...typescriptOverrides.parserOptions,
-        parser: typescriptOverrides.parser,
-        extraFileExtensions: ['.vue'],
-      },
-      plugins: typescriptOverrides.plugins,
-      extends: typescriptOverrides.extends,
-      rules: typescriptOverrides.rules
-    },
-    {
-      files: ['*.vue'],
-      rules: Object.assign({},{
-        '@typescript-eslint/explicit-function-return-type': 0,
-        '@typescript-eslint/explicit-module-boundary-types': 0
-      })
-    },
-    {
-      files: ['shims-tsx.d.ts'],
-      rules: {
-        '@typescript-eslint/no-empty-interface': 0,
-        '@typescript-eslint/no-explicit-any': 0,
-        '@typescript-eslint/no-unused-vars': 0
-      }
-    }
-  ]
+  overrides: vueTsConfig.overrides
 };
